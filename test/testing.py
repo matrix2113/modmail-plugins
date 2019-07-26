3b792cedcf120b9c203e70fea4becf54f8872b7e
Matrix <49132750+matrix2113@users.noreply.github.com>
2019-07-26 17:18:34 -0400

    Update testing.py

--- test/testing.py
+++ test/testing.py
@@ -6,17 +6,16 @@ class testing(commands.Cog):
     Testing
     """
     def __init__(self, bot):
+        
         self.bot = bot
 
     @commands.group(name="testing", invoke_without_command=True)
-    @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
-    async def dbug(self, ctx):
+    async def testing(self, ctx):
         """Testing Command"""
 
         await ctx.send_help(ctx.command)
         
     @testing.command(name="testing")
-    @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
     async def test(self, ctx):
         """Test Command"""
         await ctx.send('Testing Works')
