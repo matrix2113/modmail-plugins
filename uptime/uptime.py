619611f671098850b60cc8556bfce396b3742c84
Matrix <49132750+matrix2113@users.noreply.github.com>
2019-07-27 07:58:03 -0400

    Update uptime.py

--- uptime/uptime.py
+++ uptime/uptime.py
@@ -1,6 +1,5 @@
 import discord
-from discord.ext import commands
-from discord import Embed
+from discord.ext import commands, Embed
 from datetime import datetime
 from core.models import PermissionLevel
 from core.decorators import trigger_typing
