ba548b6c632213f4a0011b4e2507c0757d7b8833
Matrix <49132750+matrix2113@users.noreply.github.com>
2020-02-23 15:03:53 -0500

    Update id.py

--- id/id.py
+++ id/id.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-
+from core import checks
 
 class ThreadID(commands.Cog):
 	def __init__(self, bot):
@@ -8,6 +8,7 @@ class ThreadID(commands.Cog):
 		
 		
 	@commands.command()
+	@checks.thread_only()
 	async def id(self, ctx):
 		"""Returns the Recipient's ID"""
 		await ctx.send(ctx.thread.id)
