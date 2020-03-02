5a3b9bee574fbe51f7a143609af5270ce858d736
Matrix <49132750+matrix2113@users.noreply.github.com>
2020-03-02 18:08:19 -0500

    Update partnerships.py

--- partnerships/partnerships.py
+++ partnerships/partnerships.py
@@ -15,6 +15,11 @@ class Partnerships(commands.Cog):
       if ctx.invoked_subcommand is None:
         return
         
-        
+    @partnership.command()
+    @commands.has_any_role(364843179888869376, 472812430544863235)
+    async def small(self, ctx, user: discord.Member=None):
+      await ctx.send("test")
+      
+
 def setup(bot):
   bot.add_cog(Partnerships(bot))
