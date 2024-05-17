b67d3951f0332ab7b5c0da0bd882e9b0a9e7d815
anderson <andy@ondrya.com>
2024-05-17 12:54:58 -0400

    Update with async

--- id/id.py
+++ id/id.py
@@ -2,17 +2,17 @@ import discord
 from discord.ext import commands
 from core import checks
 
-class ThreadID(commands.Cog):
-	def __init__(self, bot):
-		self.bot = bot
-		
-		
-	@commands.command()
-	@checks.thread_only()
-	async def id(self, ctx):
-		"""Returns the Recipient's ID"""
-		await ctx.send(ctx.thread.id)
-		
 
-def setup(bot):
-	bot.add_cog(ThreadID(bot))
+class ThreadID(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @checks.thread_only()
+    async def id(self, ctx):
+        """Returns the Recipient's ID"""
+        await ctx.send(ctx.thread.id)
+
+
+async def setup(bot):
+    await bot.add_cog(ThreadID(bot))
